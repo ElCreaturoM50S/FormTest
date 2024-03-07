c9300617c6eadd7ba8577729ab584ee7f0b2cf7d
user2e23 <113674920+ElCreaturoM50S@users.noreply.github.com>
2024-03-07 23:07:37 +0100

    Troche kodou

--- app/src/main/java/com/example/formtest/MainActivity.kt
+++ app/src/main/java/com/example/formtest/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.formtest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Spinner
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var et: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,5 +26,17 @@ class MainActivity : AppCompatActivity() {
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spinner.adapter = adapter
         }
+
+        val et: EditText = findViewById(R.id.duzo)
+        val bs: Button = findViewById(R.id.buttonSend)
+
+        val rege = Regex("^[test].+")
+
+        bs.setOnClickListener{
+            if (rege.matches(et.text)) {
+                Toast.makeText(this, et.text, Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
